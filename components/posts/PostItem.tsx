02ec312534f8dd070a746ca3ebbd524658b80014
Raziel Aleman <razielali10@.com>
2024-01-20 22:41:09 -0900

    Added post view and like functionality

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -4,18 +4,20 @@ import { formatDistanceToNowStrict } from "date-fns";
 import { useRouter } from "next/router";
 import { useCallback, useMemo } from "react";
 import Avatar from "../Avatar";
-import { AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
+import { AiOutlineHeart, AiFillHeart, AiOutlineMessage } from "react-icons/ai";
+import useLike from "@/hooks/useLike";
 
 interface PostItemProps {
   data: Record<string, any>;
   userId?: string;
 }
 
-const PostItems: React.FC<PostItemProps> = ({ data, userId }) => {
+const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
   const router = useRouter();
   const loginModal = useLoginModal();
 
-  const { data: curentUser } = useCurrentUser();
+  const { data: currentUser } = useCurrentUser();
+  const { hasLiked, toggleLike } = useLike({ postId: data.id, userId });
 
   const goToUser = useCallback(
     (event: any) => {
@@ -33,9 +35,14 @@ const PostItems: React.FC<PostItemProps> = ({ data, userId }) => {
   const onLike = useCallback(
     (event: any) => {
       event.stopPropagation();
-      loginModal.onOpen();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      toggleLike();
     },
-    [loginModal]
+    [loginModal, currentUser, toggleLike]
   );
 
   const createdAt = useMemo(() => {
@@ -46,6 +53,8 @@ const PostItems: React.FC<PostItemProps> = ({ data, userId }) => {
     return formatDistanceToNowStrict(new Date(data.createdAt));
   }, [data?.createdAt]);
 
+  const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
+
   return (
     <div
       onClick={goToPost}
@@ -55,31 +64,35 @@ const PostItems: React.FC<PostItemProps> = ({ data, userId }) => {
         <Avatar userId={data.user.id} />
         <div>
           <div className="flex flex-row items-center gap-2">
-            <p onClick={goToUser} className="text-white font-semibold cursor-pointer hover:underline">
+            <p
+              onClick={goToUser}
+              className="text-white font-semibold cursor-pointer hover:underline"
+            >
               {data.user.name}
             </p>
-            <span onClick={goToUser} className="text-neutral-500 cursor-pointer hover:underline hidden md:block">
+            <span
+              onClick={goToUser}
+              className="text-neutral-500 cursor-pointer hover:underline hidden md:block"
+            >
               @{data.user.username}
             </span>
-            <span className="text-neutral-500 text-sm">
-              {createdAt}
-            </span>
+            <span className="text-neutral-500 text-sm">{createdAt}</span>
           </div>
-          <div className="text-white mt-1">
-            {data.body}
-          </div>
+          <div className="text-white mt-1">{data.body}</div>
           <div className="flex flex-row items-center mt-3 gap-10">
             <div className="flex flex-row itmes-center text-neutral-500 gap-2 cursor-pointer transition hover:text-sky-500">
               <AiOutlineMessage size={20} />
-              <p>
-                {data.comments?.length || 0}
-              </p>
+              <p>{data.comments?.length || 0}</p>
             </div>
-            <div onClick={onLike} className="flex flex-row itmes-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500">
-              <AiOutlineHeart size={20} />
-              <p>
-                {data.comments?.length || 0}
-              </p>
+            <div
+              onClick={onLike}
+              className="flex flex-row itmes-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
+            >
+              <LikeIcon
+                size={20}
+                color={hasLiked ? "red" : ""}
+              />
+              <p>{data.likedIds.length || 0}</p>
             </div>
           </div>
         </div>
@@ -88,4 +101,4 @@ const PostItems: React.FC<PostItemProps> = ({ data, userId }) => {
   );
 };
 
-export default PostItems;
+export default PostItem;
